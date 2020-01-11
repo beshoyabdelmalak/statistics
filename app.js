e892a8923c64a223d7234a2cb3e30a0b4c95f35e
beshoy <beshoyibraheem@gmail.com>
2020-01-11 15:23:51 +0100

    add one more stats

--- app.js
+++ app.js
@@ -51,35 +51,6 @@ const excludedUsers = [
 
 //Routes
 app.get("/", async function(req, res){
-  // users = await Ctgamestudio.aggregate([
-  //     { $lookup:
-  //       { from: "users",
-  //         localField: "owner",
-  //         foreignField: "_id",
-  //         as: "new_user"
-  //       }
-  //     },
-  //     { $project:
-  //       { new_user : {name: 1},
-  //         owner: 1
-  //       }
-  //     },
-  //     { $group:
-  //       { _id: "$owner",
-  //         name: { $first: "$new_user.name" }
-  //       }
-  //     },
-  //     {
-  //       $match:
-  //       {
-  //         name: {
-  //           $not: { $size: 0 }
-  //         }
-  //       }
-  //     }
-  // ]);
-  // console.log(users[0].name)
-  // users = await User.find();
   user_ids = await Ctgamestudio.distinct("owner");
   users = await User.find({_id : {$in : user_ids}, email : {$nin : excludedUsers}}).select('name')
   levels = await Ctgamestudio.distinct("level");
@@ -87,23 +58,149 @@ app.get("/", async function(req, res){
 
 });
 
-app.post("/", (req, res) => {
-  console.log(req.body.user)
-  console.log(req.body.level)
-  let idToSearch = mongoose.Types.ObjectId(req.body.user);
-  let levelToSearch = String(req.body.level);
-  // let idToSearch = mongoose.Types.ObjectId("5badc8e4ad41aedfe7363962");
-  Ctgamestudio.find({
-    owner: idToSearch,
-    level: levelToSearch,
-    title: { $ne: "get" }
-  }).exec().then(result => {
-    data = process.process(result);
-    if(data){
-      res.render("index", { data: data});
-    }
-    res.render("index", { error: true});
-  })
+app.post("/", async function(req, res) {
+  if (!req.body.user){
+    // console.log(req.body.level)
+    var data = await get_level_data(String(req.body.level))
+    console.log(data[0]);
+    res.render("index", {data: data});
+  }else{
+    // console.log(req.body.level)
+    let idToSearch = mongoose.Types.ObjectId(req.body.user);
+    let levelToSearch = String(req.body.level);
+    // let idToSearch = mongoose.Types.ObjectId("5badc8e4ad41aedfe7363962");
+    Ctgamestudio.find({
+      owner: idToSearch,
+      level: levelToSearch,
+      title: { $ne: "get" }
+    }).exec().then(result => {
+      data = process.process(result);
+      if(data){
+        res.render("index", { data: data});
+      }
+      res.render("index", { error: true});
+    })
+  }
 });
+
+async function get_level_data(level){
+  // get the number of levels most of the users have passed
+  // and define the users passed more thean this number as high success
+  // otherwise low success
+  var levels_most_users_passed = number_of_levels();
+  var high_success_users = await get_users_data(level, "$gt");
+  var low_success_users = await get_users_data(level, '$lt');
+  var average_success_users = await get_users_data(level, '$eq');
+  var trace1 = {
+    x: ['creates', 'changes', 'runs', 'time'],
+    y: high_success_users,
+    name: 'High success',
+    marker: { color: 'rgb(85,202,106)' },
+    type: 'bar'
+  };
+
+  var trace2 = {
+    x: ['creates', 'changes', 'runs', 'time'],
+    y: average_success_users,
+    name: 'Average',
+    marker: { color: 'rgb(27,141,253)' },
+    type: 'bar'
+  };
+
+  var trace3 = {
+    x: ['creates', 'changes', 'runs', 'time'],
+    y: low_success_users,
+    name: 'Low success',
+    marker: { color: 'rgb(234,54,81)' },
+    type: 'bar'
+  };
+
+  var data = [trace1, trace2, trace3];
+  var layout = {
+    title: "LEVEL " + level,
+    barmode: 'group'
+  };
+  var plot = [data, layout];
+  return plot;
+}
+
+async function get_users_data(level, type_of_users){
+  var included_users= await User.find({email :{$nin :excludedUsers}}).select("_id");
+  var included_users_ids = included_users.map(a => a._id);
+  var users_ids =
+    await Ctgamestudio.aggregate(
+      [{
+        $match: {
+          title: "win",
+          owner : {$in : included_users_ids}
+        }
+      },
+      {
+        $group:
+          { _id: { owner: "$owner", level: "$level" } }
+      },
+      {
+        $group: { _id: "$_id.owner", count: { $sum: 1 } }
+      },
+        { $match: { count: { [type_of_users] : 5 } } }
+      ]).exec().then(result => {
+        return result.map(a => a._id);;
+      });
+  var users_data = await Ctgamestudio.aggregate(
+    [{
+      $match: {
+        level: level,
+        owner: { $in: users_ids }
+      }
+    },
+    {
+      $group:
+        { _id: "$owner", logs: { $push: "$$ROOT" } }
+    }
+    ]).exec().then(result => {
+      if (result){
+        data = process.get_level_data(result);
+        return data;
+      }
+    });
+  return users_data;
+}
+
+async function number_of_levels(){
+  levels_passed_by_each_user = await Ctgamestudio.aggregate(
+    [{
+        $match: { title: "win" }
+      },
+      { $group:
+        { _id: { owner: "$owner", level: "$level" }}
+      },
+      { $group: {
+        _id: "$_id.owner",
+        count: { $sum: 1 }
+        }
+    }]
+  );
+  let number_of_levels = levels_passed_by_each_user.map(a => a.count);
+  return get_most_frequent_levels_passed(number_of_levels);
+}
+
+function get_most_frequent_levels_passed(array) {
+  if (array.length == 0)
+    return null;
+  var modeMap = {};
+  var maxEl = array[0], maxCount = 1;
+  for (var i = 0; i < array.length; i++) {
+    var el = array[i];
+    if (modeMap[el] == null)
+      modeMap[el] = 1;
+    else
+      modeMap[el]++;
+    if (modeMap[el] > maxCount) {
+      maxEl = el;
+      maxCount = modeMap[el];
+    }
+  }
+  return maxEl;
+}
 
 app.listen(3000)
--- processing.js
+++ processing.js
@@ -27,40 +27,40 @@ module.exports = {
         if (log.title == "change" && log.event.includes("var_create")) {
           number_of_blocks += 1;
           changes.push(number_of_blocks);
-          changes_time.push(time_diff(begin_time, log.publishedDate))
+          changes_time.push(time_diff(begin_time, log.publishedDate, true))
         }
         // if blocks were created, add the number of them to the total blocks number of the program
         else if (log.title == "change" && log.event.includes("create")) {
           let event = JSON.parse(log.event);
           number_of_blocks += event.ids.length;
           changes.push(number_of_blocks);
-          changes_time.push(time_diff(begin_time, log.publishedDate))
+          changes_time.push(time_diff(begin_time, log.publishedDate, true))
         }
         // if blocks were deleted, subtract the number of deleted from the total number of blocks
         else if (log.title == "change" && log.event.includes("delete")) {
           let event = JSON.parse(log.event);
           number_of_blocks -= event.ids.length;
           changes.push(number_of_blocks);
-          changes_time.push(time_diff(begin_time, log.publishedDate))
+          changes_time.push(time_diff(begin_time, log.publishedDate, true))
         }
         else if (log.title == "change" && (log.event.includes("change") || log.event.includes("move"))) {
           changes.push(number_of_blocks);
-          changes_time.push(time_diff(begin_time, log.publishedDate))
+          changes_time.push(time_diff(begin_time, log.publishedDate, true))
         }
         else if (log.title == "win") {
           changes.push(number_of_blocks);
-          changes_time.push(time_diff(begin_time, log.publishedDate))
+          changes_time.push(time_diff(begin_time, log.publishedDate, true))
           wins.push(number_of_blocks);
-          wins_time.push(time_diff(begin_time, log.publishedDate))
+          wins_time.push(time_diff(begin_time, log.publishedDate, true))
           throw new Error();
         }
         // if the code was tests, mark the time of the test
         else if (log.title == "test") {
           // tests.push(number_of_blocks);
           changes.push(number_of_blocks);
-          changes_time.push(time_diff(begin_time, log.publishedDate))
+          changes_time.push(time_diff(begin_time, log.publishedDate, true))
           tests.push(number_of_blocks);
-          tests_time.push(time_diff(begin_time, log.publishedDate))
+          tests_time.push(time_diff(begin_time, log.publishedDate, true))
         }
       });
     }
@@ -80,6 +80,90 @@ module.exports = {
       }
     };
     return [data, layout];
+  },
+  get_level_data : function (users){
+    // return the average values in this sequence
+    // creates, changes, runs, time
+    let average_values = []
+    let number_of_creates_per_user = [];
+    let number_of_changes_per_user = [];
+    let number_of_runs_per_user = [];
+    let time_per_user = [];
+    if (users.length == 0) {
+      return null;
+    }
+    users.forEach(function (user) {
+      console.log(user._id)
+      var first = true;
+      var begin_time = new Date();
+      let number_of_creates = 0;
+      let number_of_changes = 0;
+      let number_of_runs = 0;
+      let time = [];
+      try{
+        user.logs.forEach(function (log){
+          // for the first block, set the time
+          if (first) {
+            begin_time = log.publishedDate;
+            first = false;
+          }
+          // if it reate a variable, count it as adding one block
+          if (log.title == "change" && log.event.includes("var_create")) {
+            number_of_creates++;
+            time.push(time_diff(begin_time, log.publishedDate, false));
+          }
+          // if blocks were created, add the number of them to the total blocks number of the program
+          else if (log.title == "change" && log.event.includes("create")) {
+            number_of_creates++;
+            time.push(time_diff(begin_time, log.publishedDate, false));
+          }
+          else if (log.title == "change" && (log.event.includes("change") || log.event.includes("move"))) {
+            number_of_changes++;
+            time.push(time_diff(begin_time, log.publishedDate, false));
+          }
+          else if (log.title == "win") {
+            number_of_runs++;
+            time.push(time_diff(begin_time, log.publishedDate, false));
+            // to break after the first win
+            throw new Error();
+          }
+          // if the code was tests, mark the time of the test
+          else if (log.title == "test") {
+            number_of_runs++;
+            time.push(time_diff(begin_time, log.publishedDate, false));
+          }
+        });
+      }catch(e){
+      }
+      number_of_changes_per_user.push(number_of_changes);
+      number_of_creates_per_user.push(number_of_creates);
+      number_of_runs_per_user.push(number_of_runs);
+      // time in minutes till the first win
+      time_to_first_win = time[time.length - 1] / 60
+      if (time_to_first_win > 45 ){
+        // if the time is more than 45 minutes
+        // like in the case of the user with the id 5bbdbb2cc40a6a07cc833af8
+        // where the first win was after nearly a month of the first time
+        // she opened the first level
+        time_per_user.push(45);
+      }else{
+        time_per_user.push(time_to_first_win);
+      }
+    });
+    // function to calculate average of for all users
+    const arrAvg = arr => arr.reduce((a, b) => a + b, 0) / arr.length
+
+    average_values.push(arrAvg(number_of_creates_per_user).toFixed(1));
+    average_values.push(arrAvg(number_of_changes_per_user).toFixed(1));
+    average_values.push(arrAvg(number_of_runs_per_user).toFixed(1));
+    average_values.push(arrAvg(time_per_user).toFixed(1));
+
+    // console.log(number_of_changes_per_user)
+    // console.log(number_of_creates_per_user)
+    // console.log(number_of_runs_per_user)
+    // console.log(time_per_user)
+    // console.log(average_values)
+    return average_values;
   }
 };
 
@@ -156,11 +240,15 @@ function plot_points(number_of_blocks, time, type){
   };
   return trace;
 }
-function time_diff(begin_time, end_time){
+function time_diff(begin_time, end_time, add_one){
   diff_ms = end_time - begin_time
   // diff = Math.round(((diff_ms % 86400000) % 3600000) / 60000);
   // time difference in seconds
   diff = diff_ms / 1000;
-  // adding one second is nessecary for the blocks given in each level
-  return diff + 1;
+  if (add_one){
+    // adding one second is nessecary for the blocks given in each level
+    return diff + 1;
+  }
+  return diff;
+
 }
